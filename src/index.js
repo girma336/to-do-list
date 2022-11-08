3018bf0bc1660ac651e93897bf610ddc3e95b692
girma <girmatarekegn.gi@gmail.com>
2022-11-08 17:57:30 +0300

    Add src/index.js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import './style.css';
 import './to-go-bg.jpg';
 
-class Todo{
+const taskInput = document.getElementById('input-task');
+const addBtn = document.getElementById('add-task');
+
+class Todo {
   constructor(description, index, completed = false) {
     this.description = description;
     this.index = index;
     this.completed = completed;
   }
 }
+
+addBtn.addEventListener('click', () => {
+  if (taskInput.value !== '') {
+    const todolist = new Todo(taskInput.value, 0);
+    localStorage.setItem('todolists', JSON.stringify(todolist));
+    taskInput.value = '';
+  }
+});
