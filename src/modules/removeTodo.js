9ac81a0bad5df8522d4181e83d9f532acc24d4af
girma <girmatarekegn.gi@gmail.com>
2022-11-10 14:23:57 +0300

    Add contante of incase of index

--- src/modules/removeTodo.js
+++ src/modules/removeTodo.js
@@ -1,13 +1,13 @@
 const removeTodo = (index) => {
   let todoList = JSON.parse(localStorage.getItem('todo'));
   todoList = todoList.filter((todo) => todo.index !== Number(index));
-  let i = 0;
+  let i = 1;
   todoList.forEach((todo) => {
     todo.index = i;
     i += 1;
   });
   localStorage.setItem('todo', JSON.stringify(todoList));
-  window.location.reload();
+   window.location.reload();
 };
 
 export default removeTodo;
